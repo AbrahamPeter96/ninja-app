21d6eba5d7be70fa82922ce6e4c2a2536137efef
mudaseriqbalshah <mudaseriqbalshah@gmail.com>
2021-09-06 20:01:39 +0500

    basic setup express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('aoa');
+const express = require('express');
+const app = express(); //set up express
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+//Listen for req
+
+app.listen(port, () => {
+  console.log('port', port);
+  console.log('Hello');
+});
