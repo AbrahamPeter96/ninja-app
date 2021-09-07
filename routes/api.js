26ba90eee9ec3dfe6faf1a7bc8c2341f71c656cf
mudaseriqbalshah <mudaseriqbalshah@gmail.com>
2021-09-07 17:37:02 +0500

    send the req body back to client

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ router.get('/ninjas', (req, res) => {
 // add new in db
 router.post('/ninjas', (req, res) => {
   console.log('Request.Body', req.body);
-  res.send({ type: 'POST' });
+  res.send({ type: 'POST', body: req.body });
 });
 // Update in db
 router.put('/ninjas/:id', (req, res) => {
