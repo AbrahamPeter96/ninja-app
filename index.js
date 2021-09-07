cada2c842fe856b6d63f2d2be4ee7df199e56343
mudaseriqbalshah <mudaseriqbalshah@gmail.com>
2021-09-07 14:50:43 +0500

    get request

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express(); //set up express
 require('dotenv').config();
 const port = process.env.PORT || 4000;
 //Listen for req
+app.get('/', (req, res) => {
+  console.log('Get');
+  res.end();
+});
 
 app.listen(port, () => {
   console.log('port', port);
